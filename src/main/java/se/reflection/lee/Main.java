31ccabc324e9c75fbfb1bdf59222cd564a3bd2b9
lee fowler <fowler.lee8@google.com>
2021-05-16 16:33:34 +0200

    implemented setObjectColor() method in Main as per Book on Reflection

--- src/main/java/se/reflection/lee/Main.java
+++ src/main/java/se/reflection/lee/Main.java
@@ -1,8 +1,35 @@
 package se.reflection.lee;
 
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
 
+
+
     }
+
+    private static void setObjectColor(Object obj, Color color) {
+        Class cls = obj.getClass();
+
+        try {
+            Method method = cls.getMethod("setColor", new Class[]{Color.class});
+            method.invoke(obj, new Object[]{color});
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(cls.getName()
+                    + " does not support method setColor(Color)");
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException("Insufficient access " +
+                    "permissions to call" + "setColor(:Color in class "
+                    + cls.getName());
+        }
+
+    }
+
 
 }
